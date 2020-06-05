d9a8b50147571ba4f25dbf2912a722431548ae06
sandrabosk <sandraboskovic@hotmail.com>
2020-06-04 20:44:30 -0400

    super basic examples with CRA

--- src/App.js
+++ src/App.js
@@ -2,23 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// 1:
+const statement = <h1> React is cool! </h1>;
+
+// 2:
+const student = {
+  firstName: 'ana',
+  lastName: 'martinez'
+};
+
+// 3:
+const { firstName, lastName } = student;
+
+// 4:
+function capitalizeFirstLetter() {
+  return `I have capitalized first letters: ${firstName[0].toUpperCase() + firstName.slice(1)} ${
+    lastName[0].toUpperCase() + lastName.slice(1)
+  }!`;
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* 1: */}
+      {statement}
+      {/* 2: */}
+      <h3>
+        Hi, {student.firstName} {student.lastName}!
+      </h3>
+      {/* 3: */}
+      <h4>
+        Here is uppercased: {firstName.toUpperCase()} {lastName.toUpperCase()}.
+      </h4>
+
+      {/* 4: */}
+      <h4>{capitalizeFirstLetter()}</h4>
+
+      <div>
+        <img
+          src='https://s3-eu-west-1.amazonaws.com/ih-materials/uploads/upload_129040de20781c45366fd2193fcbb5fd.png'
+          alt='react is cool'
+        />
+      </div>
     </div>
   );
 }
